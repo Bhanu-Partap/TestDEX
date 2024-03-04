8d513326c118fd317928dc04b4390cfd781a8818
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-03-04 10:15:59 +0530

    token0 test case

--- test/Lock.js
+++ test/Lock.js
@@ -2,36 +2,35 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Dex Test Cases", async () => {
-  let ERC20token0, ERC20token1;
+  let ERC20token0, ERC20token1, Factory;
 
   beforeEach(async () => {
   const [user1, user2, owner] = await ethers.getSigners();
 
     //              Token 1 deploynment
     let erc20token0 = await ethers.getContractFactory("erc20token");
-    let ERC20token0 = await erc20token0.deploy("Ethereum", "ETH");
+     ERC20token0 = await erc20token0.deploy("Ethereum", "ETH");
 
     //              Token 2 deploynment
     let erc20token1 = await ethers.getContractFactory("erc20token");
-    let ERC20token1 = await erc20token1.deploy("Bitcoin", "BTC");
+     ERC20token1 = await erc20token1.deploy("Bitcoin", "BTC");
 
     //              Factory Contract deploynment
     let factory = await ethers.getContractFactory("factory");
-    let Factory = await factory.deploy();
+    Factory = await factory.deploy();
     Factory.connect(owner.address )
 
   });
 
     //              Token 1 contract test cases 
     it("Should the total supply of token0 is 0", async () => {
-      let erc20token0 = await ethers.getContractFactory("erc20token");
-      let ERC20token0 = await erc20token0.deploy("Ethereum", "ETH");
       const totalSupply = await ERC20token0.totalSupply()
-      // const initialsupply = ethers.utils.parseEther("0")
       expect(totalSupply).to.equal(0);
     });
 
-    // it("Token 1 Contract test ",async ()=>{
-    //    owner = await ERC20token0.; 
-    // })
+    it("Should have to 4 decimal", async () => {
+      const decimal = await ERC20token0.decimals()
+      expect(decimal).to.equal(4);
+    });
+
 });
